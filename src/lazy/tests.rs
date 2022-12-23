7afbcb04545ace421b9db51934c8d0f48303dfee
Emil Koutanov <ekoutanov@gmail.com>
2022-12-23 16:54:26 +1100

    Removed 'lazy'

--- src/lazy/tests.rs
+++
@@ -1,9 +0,0 @@
-use crate::lazy::Lazy;
-
-#[test]
-fn lifecycle() {
-    let mut lazy = Lazy::new(|| 42);
-    assert_eq!(None, lazy.try_get());
-    assert_eq!(42, *lazy.get());
-    assert_eq!(Some(&42), lazy.try_get());
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 
 extern crate alloc;
 
-pub mod lazy;
 pub mod table;
 pub mod renderer;
 pub mod style;
